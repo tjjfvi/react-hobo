ef55910cf6d9d2dcd60bf135cad61591cf436a6f
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-22 14:04:42 -0700

    add default for generic O in observer

--- src/observer.ts
+++ src/observer.ts
@@ -1,6 +1,6 @@
 import { Readable, useReadable } from "./readable";
 
-export const observer = <I, O>(component: ((i: I) => O)): ((i: I) => O) => (i: I): O => {
+export const observer = <I, O = JSX.Element>(component: ((i: I) => O)): ((i: I) => O) => (i: I): O => {
   let c = useReadable.use(() => null);
   let d = Readable.setCur(c);
   let result = component(i);
